4d3fa0c8a23db54123e401685147bdcf78fd213e
renemonroy <rene.monroy@gmail.com>
2021-10-20 10:19:21 -0500

    Create initial constants used by Chart component

---
+++ src/components/Chart/Chart.constants.ts
@@ -0,0 +1,27 @@
+// element ids and classnames
+export const X1_AXIS_CLASSNAME = "bottom-axis";
+export const Y1_AXIS_CLASSNAME = "left-axis";
+export const BAR_CLASSNAME = "bar";
+
+// variable names
+export const X1_VALUE = "x1";
+export const Y1_VALUE = "y1";
+
+export const BORDER_RADIUS = 0;
+
+export const DEBOUNCE_DELAY = 400;
+export const SCALE_GAP = 30;
+export const AXIS_WIDTH = 0;
+export const AXIS_HEIGHT = 0;
+export const X_AXIS_SPACE = 10;
+export const Y_AXIS_SPACE = 20;
+
+export const TEXT_CLASSNAME = "litt-chart-text";
+export const SHAPE_CLASSNAME = "litt-chart-shape";
+export const LEFT_AXIS_CLASSNAME = "litt-chart-left-axis";
+export const RIGHT_AXIS_CLASSNAME = "litt-chart-right-axis";
+export const BOTTOM_AXIS_CLASSNAME = "litt-chart-bottom-axis";
+export const BG_LINES_CLASSNAME = "litt-chart-bg-lines";
+export const BG_LINE_CLASSNAME = "litt-chart-bg-line";
+export const CHART_VERTICAL_BARS = "litt-chart-vertical-bars";
+export const CHART_LINE = "litt-chart-line";
